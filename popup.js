01e93cbd958d4528593907b09d28c3e04a66a5b5
Aygul <gulya@gmx.net>
2025-12-28 21:18:47 +0100

    refactor: popup title text from "Hello CI" to "Hello"

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("title").textContent = "Hello CI";
+    document.getElementById("title").textContent = "Hello";
+    console.log("Change Hello CI to Hello");
 });
