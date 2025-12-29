ceed647898342d53a0cf3199f574b18ab541e3e2
Aygul <gulya@gmx.net>
2025-12-29 18:09:29 +0100

    feat: add button with click event to update its text

--- popup.js
+++ popup.js
@@ -2,3 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("title").textContent = "Hello";
     console.log("Change Hello CI to Hello");
 });
+const button = document.getElementById("testbutton");
+document.addEventListener("click", () => {
+    button.textContent = "Danke fuer den Klick";
+});
